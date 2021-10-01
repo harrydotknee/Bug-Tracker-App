fea99176ae513982f97c2c8905b1f187c98e77cc
harrydotknee <harry@veeqo.com>
2021-10-01 10:01:50 +0100

    add bugs database table, associate with project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
     has_and_belongs_to_many :users
     belongs_to :author, class_name: 'User'
+    has_many :bugs
 end
--- db/schema_old.rb
+++ db/schema_old.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_163019) do
+ActiveRecord::Schema.define(version: 2021_09_30_155436) do
 
   create_table "bugs", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "project_id"
   end
 
   create_table "project_users", force: :cascade do |t|
